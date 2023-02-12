9cc118f08dcf362e2984b3ebaf9074880eed52a8
anikasharma <anika17@unc.edu>
2023-02-11 21:11:53 -0500

    um

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as atest
 import speech_recognition as sr
-import nltk
-nltk.download("vader_lexicon")
-from nltk.sentiment import SentimentIntensityAnalyzer
-from nltk.tokenize import word_tokenize
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-
+ 
 sentiment_analyzer = SentimentIntensityAnalyzer()
+
 
 def transcribe_audio(audio_file):
     recognizer = sr.Recognizer()
@@ -14,19 +13,100 @@ def transcribe_audio(audio_file):
         audio = recognizer.record(source)
     return recognizer.recognize_google(audio)
 
-def analyze_sentiment(text):
-    sentiment = sentiment_analyzer.polarity_scores(text)
-    return sentiment
+
+def sentiment_scores(text):
+ 
+    # Create a SentimentIntensityAnalyzer object.
+    sid_obj = SentimentIntensityAnalyzer()
+ 
+    # polarity_scores method of SentimentIntensityAnalyzer
+    # object gives a sentiment dictionary.
+    # which contains pos, neg, neu, and compound scores.
+    sentiment_dict = sid_obj.polarity_scores(text)
+     
+    print("Here is your tone distribution! : ", sentiment_dict)
+    print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
+    print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
+    print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
+ 
+    print("Sentence Overall Rated As", end = " ")
+ 
+    # decide sentiment as positive, negative and neutral
+    if sentiment_dict['compound'] >= 0.05 :
+        print("This recording has a positive connotation!")
+ 
+    elif sentiment_dict['compound'] <= - 0.05 :
+        print("This recording has a negative connotation!")
+ 
+    else :
+        print("This recording has a neutral tone!")
 
 
+def passive(text):
+    passive_words = ["am", "is", "been", "was", "are", "be", "being"]
+    non_passive_words = ['do', 'did', 'does', 'have', 'has', 'had']
+    passive_counter = 0 
+    non_passive_counter = 0 
+    broken = text.split()
+    for i in broken:
+        if i in passive_words: 
+            passive_counter += 1
+        elif i in non_passive_words: 
+            non_passive_counter += 1 
+    if non_passive_counter!= 0:
+        passive_ratio = passive_counter/non_passive_counter
+    return passive_ratio
 
-def suggest_improvements(sentiment):
-    if sentiment["compound"] > 0.05:
-        return "The text has a positive tone. Keep it up!"
-    elif sentiment["compound"] < -0.05:
-        return "The text has a negative tone. Try to use more positive words and phrases."
-    else:
-        return "The text has a neutral tone. Try to add some emotions to make it more interesting."
+def confidence(text):
+    not_confident = ["like", "umm", "um", "but", "uh", "you know", "i mean", "so", "just", "basically", "i guess"]
+    weakling_counter = 5
+    broken = text.split()
+    for i in broken: 
+        if i in not_confident:
+            weakling_counter -= 1
+    return weakling_counter
+
+def get_formality_score(text):
+    formal_words = ["please", "kindly", "thank you", "sincerely", "yours truly"]
+    informal_words = ["gonna", "wanna", "ain't", "don't", "can't"]
+    formal_counter = 0
+    informal_counter = 0
+    formality_ratio = 0
+
+    broken = text.split()
+    for i in broken:
+        if i in formal_words:
+            formal_counter += 1
+        elif i in informal_words:
+            informal_counter -= 1
+    if informal_counter!= 0: 
+        formality_ratio = formal_counter/informal_counter
+    return formality_ratio
+
+
+def suggestions(text):
+
+    if passive(text) > 1: 
+        print("Your tone is too passive. Use more active words. Examples of active words are do, did, does.")
+    
+    if passive(text) < 1: 
+        print("Your recording uses active verbs and non-passive words for the most part! Good job!")
+    
+    if confidence(text) <= 0: 
+        print("The tone could be more confident. Avoid using filler words and take pauses instead of rambling on with 'ums' and 'buts'. You got this! :) ")
+
+    if confidence(text) > 0: 
+        print("Your tone has the right amount of confidence! Keep it up!")
+    
+    if get_formality_score(text) > 1:
+        print("The overall tone of your recording is formal!")
+    
+    if get_formality_score(text) < 1: 
+        print("The overall tone of your recording is informal.")
+    
+    if get_formality_score(text) == 0: 
+        print("The overall tone of your recording in terms of formality is neutral!")
+    
 
 def main():
     st.title("Awesome app")
@@ -34,44 +114,141 @@ def main():
     audio_file = st.file_uploader("", type=["wav", "mp3", "aac"])
     if audio_file:
         text = transcribe_audio(audio_file)
-        st.write("Transcription!!:" + text)
-        sentiment = analyze_sentiment(text)
-        improvement_suggestion = suggest_improvements(sentiment)
-        st.success(improvement_suggestion)
+        st.write("Your file has run successfully!")
+        overall = print(sentiment_scores(text))
+        more_suggestions = print(suggestions(text))
+        st.write(overall)
+        st.write(more_suggestions)
 
 if __name__ == '__main__':
     main()
 
+
+atest.html(
+    import streamlit as st
+import streamlit.components.v1 as atest
+import speech_recognition as sr
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+sentiment_analyzer = SentimentIntensityAnalyzer()
+
+
+def transcribe_audio(audio_file):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio = recognizer.record(source)
+    return recognizer.recognize_google(audio)
+
+
+def sentiment_scores(text):
+ 
+    # Create a SentimentIntensityAnalyzer object.
+    sid_obj = SentimentIntensityAnalyzer()
+ 
+    # polarity_scores method of SentimentIntensityAnalyzer
+    # object gives a sentiment dictionary.
+    # which contains pos, neg, neu, and compound scores.
+    sentiment_dict = sid_obj.polarity_scores(text)
+     
+    print("Here is your tone distribution! : ", sentiment_dict)
+    print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
+    print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
+    print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
+ 
+    print("Sentence Overall Rated As", end = " ")
+ 
+    # decide sentiment as positive, negative and neutral
+    if sentiment_dict['compound'] >= 0.05 :
+        print("This recording has a positive connotation!")
+ 
+    elif sentiment_dict['compound'] <= - 0.05 :
+        print("This recording has a negative connotation!")
+ 
+    else :
+        print("This recording has a neutral tone!")
+
+
 def passive(text):
     passive_words = ["am", "is", "been", "was", "are", "be", "being"]
     non_passive_words = ['do', 'did', 'does', 'have', 'has', 'had']
-    passive_score = 0
-    tokens = word_tokenize(text)
-    for token in tokens: 
-        if token in passive_words: 
-            passive_score += 1
-        elif token in non_passive_words: 
-            passive_score -= 1
+    passive_counter = 0 
+    non_passive_counter = 0 
+    broken = text.split()
+    for i in broken:
+        if i in passive_words: 
+            passive_counter += 1
+        elif i in non_passive_words: 
+            non_passive_counter += 1 
+    if non_passive_counter!= 0:
+        passive_ratio = passive_counter/non_passive_counter
+    return passive_ratio
 
-    return passive_score
-
-def suggestions(text):
-    suggestions = []
-
-    if passive(text) > 10: 
-        suggestions.append("Your tone is too passive. Use more active words. Examples of active words are do, did, does")
-
+def confidence(text):
+    not_confident = ["like", "umm", "um", "but", "uh", "you know", "i mean", "so", "just", "basically", "i guess"]
+    weakling_counter = 5
+    broken = text.split()
+    for i in broken: 
+        if i in not_confident:
+            weakling_counter -= 1
+    return weakling_counter
 
 def get_formality_score(text):
     formal_words = ["please", "kindly", "thank you", "sincerely", "yours truly"]
     informal_words = ["gonna", "wanna", "ain't", "don't", "can't"]
+    formal_counter = 0
+    informal_counter = 0
+    formality_ratio = 0
 
-    formality_score = 0
-    tokens = word_tokenize(text)
-    for token in tokens:
-        if token in formal_words:
-            formality_score += 1
-        elif token in informal_words:
-            formality_score -= 1
+    broken = text.split()
+    for i in broken:
+        if i in formal_words:
+            formal_counter += 1
+        elif i in informal_words:
+            informal_counter -= 1
+    if informal_counter!= 0: 
+        formality_ratio = formal_counter/informal_counter
+    return formality_ratio
+
+
+def suggestions(text):
+
+    if passive(text) > 1: 
+        print("Your tone is too passive. Use more active words. Examples of active words are do, did, does.")
     
-    return formality_score
+    if passive(text) < 1: 
+        print("Your recording uses active verbs and non-passive words for the most part! Good job!")
+    
+    if confidence(text) <= 0: 
+        print("The tone could be more confident. Avoid using filler words and take pauses instead of rambling on with 'ums' and 'buts'. You got this! :) ")
+
+    if confidence(text) > 0: 
+        print("Your tone has the right amount of confidence! Keep it up!")
+    
+    if get_formality_score(text) > 1:
+        print("The overall tone of your recording is formal!")
+    
+    if get_formality_score(text) < 1: 
+        print("The overall tone of your recording is informal.")
+    
+    if get_formality_score(text) == 0: 
+        print("The overall tone of your recording in terms of formality is neutral!")
+    
+
+def main():
+    st.title("Awesome app")
+    st.subheader("Upload your audio file below:")
+    audio_file = st.file_uploader("", type=["wav", "mp3", "aac"])
+    if audio_file:
+        text = transcribe_audio(audio_file)
+        st.write("Your file has run successfully!")
+        overall = print(sentiment_scores(text))
+        more_suggestions = print(suggestions(text))
+        st.write(overall)
+        st.write(more_suggestions)
+
+if __name__ == '__main__':
+    main()
+
+
+atest.html 
